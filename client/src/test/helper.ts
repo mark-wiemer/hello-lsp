659514a1dec8340f73a4bc616caee1f81b12c207
Mark Wiemer <markwiemer@outlook.com>
2024-08-10 11:05:53 -0700

    Document helper.ts

--- client/src/test/helper.ts
+++ client/src/test/helper.ts
@@ -11,37 +11,38 @@ export let editor: vscode.TextEditor;
 export let documentEol: string;
 export let platformEol: string;
 
-/**
- * Activates the vscode.lsp-sample extension
- */
-export async function activate(docUri: vscode.Uri) {
-	// The extensionId is `publisher.name` from package.json
-	const ext = vscode.extensions.getExtension('vscode-samples.lsp-sample')!;
-	await ext.activate();
-	try {
-		doc = await vscode.workspace.openTextDocument(docUri);
-		editor = await vscode.window.showTextDocument(doc);
-		await sleep(2000); // Wait for server activation
-	} catch (e) {
-		console.error(e);
-	}
+/** Activates the vscode.lsp-sample extension */
+export async function activate(docUri: vscode.Uri): Promise<void> {
+    // The extensionId is `publisher.name` from package.json
+    const ext = vscode.extensions.getExtension('vscode-samples.lsp-sample')!;
+    await ext.activate();
+    try {
+        doc = await vscode.workspace.openTextDocument(docUri);
+        editor = await vscode.window.showTextDocument(doc);
+        await sleep(2000); // Wait for server activation
+    } catch (e) {
+        console.error(e);
+    }
 }
 
-async function sleep(ms: number) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+async function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export const getDocPath = (p: string) => {
-	return path.resolve(__dirname, '../../testFixture', p);
-};
-export const getDocUri = (p: string) => {
-	return vscode.Uri.file(getDocPath(p));
+const getDocPath = (p: string): string => {
+    return path.resolve(__dirname, '../../testFixture', p);
 };
 
+/** Returns the vscode.Uri for the provided filename within the testFixture folder */
+export const getDocUri = (p: string): vscode.Uri => {
+    return vscode.Uri.file(getDocPath(p));
+};
+
+/** Replaces the content of the current file with the provided `content` string */
 export async function setTestContent(content: string): Promise<boolean> {
-	const all = new vscode.Range(
-		doc.positionAt(0),
-		doc.positionAt(doc.getText().length)
-	);
-	return editor.edit(eb => eb.replace(all, content));
+    const all = new vscode.Range(
+        doc.positionAt(0),
+        doc.positionAt(doc.getText().length),
+    );
+    return editor.edit((eb) => eb.replace(all, content));
 }
